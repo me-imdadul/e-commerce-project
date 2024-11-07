3432c3a136e3e5e859bdf1ff690e1506ba065099
Nabeel <nabeelaminlaskar@gmail.com>
2024-11-07 19:04:46 +0530

    added connection

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = ""; 
+$password = "";
+$dbname = "ecom";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
